b0b9d563da082bc6ddb4f2a3e1a193d4651f21d0
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-06-21 23:58:46 +0100

    feat: update virtual-accounts resource

--- lib/resources/virtual-accounts.resource.ts
+++ lib/resources/virtual-accounts.resource.ts
@@ -7,6 +7,10 @@ class VirtualAccounts extends KudaRequest {
     super({ ...options });
   }
 
+  private getTrackingReference({ lastName, firstName, email }: { lastName: string; firstName: string; email: string }) {
+    return this.generateTrackingReference(lastName, firstName, email);
+  }
+
   async getOne(data: { email: string; lastName: string; firstName: string }) {
     const trackingReference = this.getTrackingReference(data);
     return (await this.request({
@@ -16,10 +20,6 @@ class VirtualAccounts extends KudaRequest {
         trackingReference,
       },
     })) as IkudaResponseData;
-  }
-
-  private getTrackingReference({ lastName, firstName, email }: { lastName: string; firstName: string; email: string }) {
-    return this.generateTrackingReference(lastName, firstName, email);
   }
 
   async getBalance(data: { email: string; lastName: string; firstName: string }) {
@@ -41,13 +41,46 @@ class VirtualAccounts extends KudaRequest {
     })) as IkudaResponseData;
   }
 
-  async transactions() {}
+  async transactions(data: { email: string; lastName: string; firstName: string }) {
+    const trackingReference = this.getTrackingReference(data);
+    return (await this.request({
+      serviceType: serviceTypeEnums.ADMIN_VIRTUAL_ACCOUNT_TRANSACTIONS,
+      requestRef: this.generateRequestReference(),
+      data: { trackingReference, pageSize: 1, pageNumber: 1 },
+    })) as IkudaResponseData;
+  }
 
-  async filterTransactions() {}
+  //yet to figure out the datetype
+  async filterTransactions(data: {
+    email: string;
+    lastName: string;
+    firstName: string;
+    startDate: string;
+    endDate: string;
+  }) {
+    const trackingReference = this.generateTrackingReference(data.lastName, data.email, data.firstName);
+    return (await this.request({
+      serviceType: serviceTypeEnums.ADMIN_VIRTUAL_ACCOUNT_FILTERED_TRANSACTIONS,
+      requestRef: this.generateRequestReference(),
+      data: { trackingReference, pageSize: 1, pageNumber: 1, startDate: data.startDate, endDate: data.endDate },
+    })) as IkudaResponseData;
+  }
 
-  async fund() {}
+  async fund(data: { amount: number; narration: string; trackingReference: string }) {
+    return (await this.request({
+      serviceType: serviceTypeEnums.FUND_VIRTUAL_ACCOUNT,
+      requestRef: this.generateRequestReference(),
+      data: data,
+    })) as IkudaResponseData;
+  }
 
-  async withdraw() {}
+  async withdraw(data: { amount: number; narration: string; trackingReference: string }) {
+    return (await this.request({
+      serviceType: serviceTypeEnums.WITHDRAW_VIRTUAL_ACCOUNT,
+      requestRef: this.generateRequestReference(),
+      data: data,
+    })) as IkudaResponseData;
+  }
 }
 
 export default VirtualAccounts;
